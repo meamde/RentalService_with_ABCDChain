c338f21cc5008c5c8bde152b214060be4f4a18fa
WSH <sywang0202@naver.com>
2018-06-27 15:14:13 +0000

    RaspServer.c_v1.4

--- RaspTcp/RaspServer.c
+++ RaspTcp/RaspServer.c
@@ -14,16 +14,16 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pthread.h>
+
 #include "../CJson/cJSON.h"
+#include "RaspServer.h"
 
 #pragma warning(disable: 4996)
 #define _CRT_SECURE_NO_WARNINGS
 #define BUF_SIZE 100
 #define MAX_CLNT 256
 
-void * handle_clnt(void * arg);
-void send_blockinfo(char * blockinfo, int len);
-void error_handling(char * blockinfo);
+
 
 int clnt_cnt = 0;
 int clnt_socks[MAX_CLNT];
@@ -31,12 +31,66 @@ pthread_mutex_t mutx;
 
 int main(int argc, char *argv[])
 {
+	char buf[BUF_SIZE];
+
+	//InterAndro(argc, argv); //지금은 주석처리 , but 나중에는 andro안에서 block이 호출되어야함
+	InterBlock(argc, argv);
+		
+	return 0;
+}
+
+void InterBlock(int argc,char *argv[] ) //Acting like Client
+{
+	int sock;
+	unsigned int jsonsize;
+	char * jsonarr[BUF_SIZE];
+	unsigned char bytes[4];
+	unsigned long n = 175;
+	
+	int str_len;
+	struct sockaddr_in serv_adr;
+
+	if(argc!=3) {
+		printf("Usage : %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
+	
+	sock = socket(PF_INET, SOCK_STREAM, 0);   
+	if(sock==-1)
+		error_handling("socket() error");
+	
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_adr.sin_port = htons(atoi(argv[2]));
+	
+	if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
+		error_handling("connect() error!");
+	else
+		puts("Connected...........");
+	
+	while(1) 
+	{
+		write(sock, bytes, sizeof(jsonarr)); //Request for jsonlenth
+		str_len = read(sock, jsonarr, BUF_SIZE-1);
+		jsonarr[str_len] = 0;
+		printf("bytes from BlockChain: %s", bytes);
+	}
+	
+	close(sock);
+
+}
+
+
+void InterAndro(int argc,char *argv[] )
+{
 	int serv_sock, clnt_sock;
 	struct sockaddr_in serv_adr, clnt_adr;
 	socklen_t clnt_adr_sz;
 	pthread_t t_id;
 	
-	if(argc != 2) {
+	if(argc != 2) 
+	{
 		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
@@ -62,6 +116,9 @@ int main(int argc, char *argv[])
 		
 		pthread_mutex_lock(&mutx);
 		clnt_socks[clnt_cnt++] = clnt_sock;
+
+		
+		
 		pthread_mutex_unlock(&mutx);
 	
 		pthread_create(&t_id, NULL, handle_clnt, (void*)&clnt_sock);
@@ -69,7 +126,7 @@ int main(int argc, char *argv[])
 		printf("Connected client IP: %s \n", inet_ntoa(clnt_adr.sin_addr));
 	}
 	close(serv_sock);
-	return 0;
+	
 }
 	
 void * handle_clnt(void * arg)
@@ -97,6 +154,7 @@ void * handle_clnt(void * arg)
 	return NULL;
 }
 
+
 void send_blockinfo(char * blockinfo, int len)   // send to all
 {
 	int i;
@@ -106,9 +164,11 @@ void send_blockinfo(char * blockinfo, int len)   // send to all
 	pthread_mutex_unlock(&mutx);
 }
 
+
 void error_handling(char * blockinfo)
 {
 	fputs(blockinfo, stderr);
 	fputc('\n', stderr);
 	exit(1);
 }
+
