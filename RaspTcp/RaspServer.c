0f7818ac739e5954c79874e24cd1fce0f1254941
WSH <sywang0202@naver.com>
2018-06-27 02:23:01 +0000

    RaspServer_v1.2

--- RaspTcp/RaspServer.c
+++ RaspTcp/RaspServer.c
@@ -1,5 +1,6 @@
 //
-//  RaspServer.cpp
+//  RaspServer.c
+//	(a.k.a main.c)
 //  Private_Blockchain_forDB
 //
 //  Created by Seung Hyun Wang on 2018. 6. 19..
@@ -16,15 +17,14 @@
 
 #pragma warning(disable: 4996)
 #define _CRT_SECURE_NO_WARNINGS
-
 #define BUF_SIZE 100
 #define MAX_CLNT 256
 
 void * handle_clnt(void * arg);
-void send_msg(char * msg, int len);
-void error_handling(char * msg);
+void send_blockinfo(char * blockinfo, int len);
+void error_handling(char * blockinfo);
 
-int clnt_cnt=0;
+int clnt_cnt = 0;
 int clnt_socks[MAX_CLNT];
 pthread_mutex_t mutx;
 
@@ -34,13 +34,14 @@ int main(int argc, char *argv[])
 	struct sockaddr_in serv_adr, clnt_adr;
 	socklen_t clnt_adr_sz;
 	pthread_t t_id;
-	if(argc!=2) {
+	
+	if(argc != 2) {
 		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
   
 	pthread_mutex_init(&mutx, NULL);
-	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
 
 	memset(&serv_adr, 0, sizeof(serv_adr));
 	serv_adr.sin_family=AF_INET; 
@@ -49,16 +50,17 @@ int main(int argc, char *argv[])
 	
 	if(bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr))==-1)
 		error_handling("bind() error");
+	
 	if(listen(serv_sock, 5)==-1)
 		error_handling("listen() error");
 	
 	while(1)
 	{
 		clnt_adr_sz=sizeof(clnt_adr);
-		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
 		
 		pthread_mutex_lock(&mutx);
-		clnt_socks[clnt_cnt++]=clnt_sock;
+		clnt_socks[clnt_cnt++] = clnt_sock;
 		pthread_mutex_unlock(&mutx);
 	
 		pthread_create(&t_id, NULL, handle_clnt, (void*)&clnt_sock);
@@ -71,20 +73,20 @@ int main(int argc, char *argv[])
 	
 void * handle_clnt(void * arg)
 {
-	int clnt_sock=*((int*)arg);
-	int str_len=0, i;
-	char msg[BUF_SIZE];
+	int clnt_sock =* ((int*)arg);
+	int str_len = 0, i;
+	char blockinfo[BUF_SIZE];
 	
-	while((str_len=read(clnt_sock, msg, sizeof(msg)))!=0)
-		send_msg(msg, str_len);
+	while((str_len = read(clnt_sock, blockinfo, sizeof(blockinfo))) != 0)
+		send_blockinfo(blockinfo, str_len);
 	
 	pthread_mutex_lock(&mutx);
-	for(i=0; i<clnt_cnt; i++)   // remove disconnected client
+	for(i = 0; i < clnt_cnt; i++)   // remove disconnected client
 	{
-		if(clnt_sock==clnt_socks[i])
+		if(clnt_sock == clnt_socks[i])
 		{
-			while(i++<clnt_cnt-1)
-				clnt_socks[i]=clnt_socks[i+1];
+			while(i++ < clnt_cnt-1)
+				clnt_socks[i] = clnt_socks[i+1];
 			break;
 		}
 	}
@@ -93,17 +95,19 @@ void * handle_clnt(void * arg)
 	close(clnt_sock);
 	return NULL;
 }
-void send_msg(char * msg, int len)   // send to all
+
+void send_blockinfo(char * blockinfo, int len)   // send to all
 {
 	int i;
 	pthread_mutex_lock(&mutx);
-	for(i=0; i<clnt_cnt; i++)
-		write(clnt_socks[i], msg, len);
+	for(i = 0; i < clnt_cnt; i++)
+		write(clnt_socks[i], blockinfo, len);
 	pthread_mutex_unlock(&mutx);
 }
-void error_handling(char * msg)
+
+void error_handling(char * blockinfo)
 {
-	fputs(msg, stderr);
+	fputs(blockinfo, stderr);
 	fputc('\n', stderr);
 	exit(1);
 }
